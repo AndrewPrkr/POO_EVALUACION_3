608b37e03cff7891ca72076eb8b241f9a5093886
AndrewPrkr <99374418+AndrewPrkr@users.noreply.github.com>
2022-06-01 12:38:05 -0600

    Add files via upload

---
+++ EVA3_16_CUSTOM_EXCEPTIONS/src/eva3_16_custom_exceptions/EVA3_16_CUSTOM_EXCEPTIONS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_16_custom_exceptions;
+
+/**
+ *
+ * @author saeol
+ */
+public class EVA3_16_CUSTOM_EXCEPTIONS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
