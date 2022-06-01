0f26ce9a7763a0b6fa26ca2d311c7d0714f30c0e
AndrewPrkr <99374418+AndrewPrkr@users.noreply.github.com>
2022-06-01 12:31:51 -0600

    Add files via upload

---
+++ EVA3_10_FINALLY/src/eva3_10_finally/EVA3_10_FINALLY.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_10_finally;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author saeol
+ */
+public class EVA3_10_FINALLY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Scanner entrada = new Scanner(System.in);
+       
+       try{
+        System.out.println("INtroduce un dato entero");
+        int valor = entrada.nextInt();
+        System.out.println("El valor es " + valor);
+            
+        
+    
+    }catch(InputMismatchException e){
+        e.printStackTrace();
+        
+        
+    }finally{
+        //no importa la excepcion el finally siempre va a ejecutarse
+        //aqui va codigo seguro o muy importante en caso de que el programa se cierre
+        System.out.println("Esta linea siempre debe ejecutarse");
+       }
+        System.out.println("fin del programa");
+}
+}
